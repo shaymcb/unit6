2bbb3de4bc248a757acbf77176a569021fd1f488
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-10 08:19:19 -0400

    Updated from Brython Server: 5/10/2018 8:19:19 AM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#9May2018
+#fileDemo.py - how to read a file
+
+file = open('engmix.txt')
+
+for line in file:
+    if 'zz' in line:
+        print(line.strip()) #strip gets rid of line break
+
 
