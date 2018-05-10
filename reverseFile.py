d4d3a4bc9c66e86c388d0c378aad23819c0a9d2d
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-10 08:44:45 -0400

    Updated from Brython Server: 5/10/2018 8:44:44 AM

--- reverseFile.py
+++ reverseFile.py
@@ -2,5 +2,13 @@
 #10May2018
 #reverseFile.py
 
-def eliFesrever(file):
-    for 
+file = open('fileDemo.py')
+
+L=[]
+for line in file:
+    L.append(line.strip('\n'))
+
+L.reverse()
+for item in L:
+    print(L)
+    
