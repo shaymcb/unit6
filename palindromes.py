4ad0405c671c0dc13cbc011acf241760736a5da9
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-10 08:50:53 -0400

    Updated from Brython Server: 5/10/2018 8:50:52 AM

--- palindromes.py
+++ palindromes.py
@@ -1,1 +1,14 @@
+#Shaylee McBride
+#10May2018
+#palindromes.py
 
+file = open('engmix.txt')
+
+def palindrome(word):
+    palindrome = ""
+    for i in range(-1,-1*len(word)-1,-1):
+        palindrome += word[i]
+    return palindrome
+    
+print(palindrome('hello'))
+    
