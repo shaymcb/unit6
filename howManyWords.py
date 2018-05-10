ceb75c2025790f4dda0fa20ef7020b2e3a4c755d
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-10 08:27:39 -0400

    Updated from Brython Server: 5/10/2018 8:27:39 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,12 @@
+#Shaylee McBride
+#10May18
+#howManyWords - how many words of each length there are
+
+file = open('engmix.txt')
+
+L = [0]*22
+for line in file:
+    L[len(line.strip())-1] += 1
+
+print(L)
 
