37aa54e1ef8fb81bd6b8bcaac08e46989b9386b7
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-11 08:27:25 -0400

    Updated from Brython Server: 5/11/2018 8:27:24 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,14 @@
+#Shaylee McBride
+#11May2018
+#fileDemo.py - how to read a file
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+
+    if line.strip()[0]=='s' and line.strip()[-1]=='m' and len(line.strip())>0:
+        print(line.strip()) #strip gets rid of line break
+    numWords += 1
+    
+print(numWords)
