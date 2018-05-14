310c77f36fd194624c0b62bf61e82405944981c8
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-14 08:14:55 -0400

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,1 +1,22 @@
 
+file = open('engmix.txt')
+"""
+word = input('Enter a word: ')
+
+inDictionary = False
+for line in file:
+    if word == line.strip():
+        inDictionary = True
+        print(word,'is in the dictionary')
+        break
+if inDictionary == False:
+    print(word,'is not in the dictionary')
+"""
+
+dictionaryList = []
+
+for line in file:
+    dictionaryList.append(line.strip())
+
+num = int(input('Enter a number: '))
+print(dictionaryList[num])
