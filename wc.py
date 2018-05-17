a4702f210943973bc9a2f8c42b4b1733c619737a
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-17 08:28:08 -0400

    Updated from Brython Server: 5/17/2018 8:28:07 AM

--- wc.py
+++ wc.py
@@ -2,4 +2,18 @@
 #16May2018
 #wc.py - lines, words, characters
 
+whatFile = input('What file: ')
+file = open(whatFile)
 
+lines = 0
+words = 0
+chars = 0
+for line in file:
+    line = line.strip()
+    lines += 1
+    words += line.count(' ')+1
+    for word in line:
+        for char in word:
+            chars += 1
+
+print(lines,words,chars)
