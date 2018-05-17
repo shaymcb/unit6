aea58ef657d86508d623bfa8bf17a0d957f655e3
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-17 08:37:44 -0400

    Updated from Brython Server: 5/17/2018 8:37:43 AM

--- anagram.py
+++ anagram.py
@@ -1,1 +1,19 @@
+#Shaylee McBride
+#17May2018
+#anagram.py
 
+file = open('engmix.txt')
+
+L = []
+original = input('enter word: ')
+
+for line in file:
+    L.append(line.strip())
+
+for letter in original:
+    for word in L:
+        if letter not in word:
+            L.remove(word)
+
+print(L)
+    
