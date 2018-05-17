434470fc08f4fe961901520db354fa67f56332ad
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-17 08:17:00 -0400

    Updated from Brython Server: 5/17/2018 8:16:59 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,10 @@
+#Shaylee McBride
+#17May2018
+#warmup17.py
 
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if 'm' in line and 'c' in line and 'b' in line and 'r' in line and 'i' in line and 'd' in line and 'e' in line:
+        print(line)
