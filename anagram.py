4b279a2ce5139748e3e18481d18d1c4cd8dcc050
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-17 08:45:23 -0400

    Updated from Brython Server: 5/17/2018 8:45:22 AM

--- anagram.py
+++ anagram.py
@@ -8,12 +8,15 @@ L = []
 original = input('enter word: ')
 
 for line in file:
-    L.append(line.strip())
+    if len(line.strip()) == len(original):
+        L.append(line.strip())
 
-for letter in original:
-    for word in L:
-        if letter not in word:
-            L.remove(word)
+i = 0
+while i <= len(L):
+    for letter in original:
+        if letter not in L[i]:
+            L.remove(L[i])
+            i -= 1
 
 print(L)
     
