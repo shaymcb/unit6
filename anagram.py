ec51dcebcc1e2bd36e83540da0709474493c1d37
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-18 08:49:08 -0400

    Updated from Brython Server: 5/18/2018 8:49:07 AM

--- anagram.py
+++ anagram.py
@@ -4,19 +4,38 @@
 
 file = open('engmix.txt')
 
-L = []
-original = input('enter word: ')
+def count(word):
+    for letter in word:
+        if letter not in lists('letters'):
+            lists('letters').append(letter)
+            lists('counts').append(1)
+        else:
+            pos = letterList.index(letter)
+            lists('counts')[pos] += 1
 
-for line in file:
-    if len(line.strip()) == len(original):
-        L.append(line.strip())
 
-i = 0
-while i <= len(L):
-    for letter in original:
-        if letter not in L[i]:
-            L.remove(L[i])
-            i -= 1
-
-print(L)
+if __name__ == '__main__':
+    data = {}
+    lists('letters') = []
+    lists('counts') = []
     
+    L = []
+    
+    count('that')
+    print(lists('counts'))
+    print(lists('letters')
+"""
+    original = input('enter word: ')
+    for line in file:
+        if len(line.strip()) == len(original):
+            L.append(line.strip())
+    
+    for word in L:
+        i = 0
+        for letter in letterList:
+            if word.count(letter) != countList(letterList.index(letter)):
+                L[i] = ''
+                i += 1
+    
+    print(L)
+     """   
