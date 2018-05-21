3a6dcc59781ab592af09b30b821ef2e543c4c6eb
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-21 08:01:35 -0400

    Update longShort.py

--- longShort.py
+++ longShort.py
@@ -5,19 +5,19 @@
 file = open('engmix.txt')
 
 letters = 'abcdefghijklmnopqrstuvwxyz'
-shortList = ['thiswouldbeaverylongshortestword']*26
+shortList = ['thiswouldbeaverylongshortestword']*26 #store a long word for all 26 letters
 longList = ['']*26
 
 for line in file:
     line = line.strip()
     
     if len(line) > 0:
-        firstletter = line[0].lower()
+        firstletter = line[0].lower() #safety
     
-    if firstletter in letters:
+    if firstletter in letters: #safety
         pos = letters.index(firstletter)  #first letter position in letter list
         
-        if len(shortList[pos]) > len(line):
+        if len(shortList[pos]) > len(line):  #if word in list right now for letter longer than this word, replace it
             shortList[pos] = line
             
         if len(longList[pos]) < len(line):
